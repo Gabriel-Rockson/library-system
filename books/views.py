a433c43faac2025459e5670e6422b390a91b8185
Gabriel Rockson <gabriel.rockson@amalitech.com>
2023-07-10 16:03:01 +0000

    feat: Add view for showing all books

--- books/views.py
+++ books/views.py
@@ -1,5 +1,12 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+from books.models import Book
 
-def all_books():
-    ...
+
+def all_books(request):
+    all_the_books = Book.objects.all()
+
+    return render(
+        request, template_name="books/books_list.html", context={"books": all_the_books}
+    )
