0918d31a3b56c549c3b5ea5c2eb1518c2d603c88
Gabriel Rockson <gabriel.rockson@amalitech.com>
2023-07-10 15:17:32 +0000

    feat: Add the Book model

--- books/models.py
+++ books/models.py
@@ -1,3 +1,36 @@
+import uuid
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Book(models.Model):
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False,
+        unique=True,
+    )
+    title = models.CharField(_("Book Title"), max_length=300, null=False, blank=False)
+    description = models.TextField(
+        _("Book Description"),
+        blank=True,
+        null=True,
+        help_text=_("Give more information about this book."),
+    )
+    date_published = models.DateField(
+        verbose_name=("Date of Publication"),
+        blank=False,
+        null=False,
+        help_text=_("When was this book published?"),
+    )
+    number_of_pages = models.PositiveIntegerField(
+        _("Number of Pages"),
+        blank=False,
+        null=False,
+        help_text=_("How many pages does this book have?"),
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.title
