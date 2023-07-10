272df8ae09c293ab74b321c1a6daa0c627cbddec
Gabriel Rockson <gabriel.rockson@amalitech.com>
2023-07-10 16:11:31 +0000

    feat(db): Change the db to use sqlite3

--- LIBRARY_MANAGEMENT_SYSTEM/settings.py
+++ LIBRARY_MANAGEMENT_SYSTEM/settings.py
@@ -77,14 +77,21 @@ WSGI_APPLICATION = "LIBRARY_MANAGEMENT_SYSTEM.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "library_db",
+#         "USER": "library_user",
+#         "PASSWORD": "testpass1234",
+#         "HOST": "localhost",
+#         "PORT": 5432,
+#     }
+# }
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "library_db",
-        "USER": "library_user",
-        "PASSWORD": "testpass1234",
-        "HOST": "localhost",
-        "PORT": 5432,
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "library_system.db",
     }
 }
 
